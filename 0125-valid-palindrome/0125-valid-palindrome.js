fef7b5509aaf7ac8a0ad515f079444fe2e349421
Aditya_Mali <113601204+Aadeetya7@users.noreply.github.com>
2025-09-17 10:04:23 +0530

    Time: 3 ms (86.76%), Space: 57.5 MB (70.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,12 +3,41 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-    let filytered = ""
-    for (let char of s) {
-        if ((char >= "a" && char <= "z") || (char >= "A" && char <= "Z") || (char >= "0" && char <= "9")) {
-            filytered += char.toLowerCase();
+    // Optimal
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        while (left < right && !isAlphanumeric(s[left])){
+            left++;
         }
+        while (left < right && !isAlphanumeric(s[right])){
+            right--
+        }
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false;
+        }
+        left++;
+        right--;
+
+    }
+    return true
+    function isAlphanumeric(char) {
+        return ((char >= "a" && char <= "z")
+            || (char >= "A" && char <= "Z")
+            || (char >= "0" && char <= "9"))
     }
 
-    return filytered === filytered.split("").reverse().join("")
+
+
+
+    // BRUTE
+    // let filytered = ""
+    // for (let char of s) {
+    //     if ((char >= "a" && char <= "z") || (char >= "A" && char <= "Z") || (char >= "0" && char <= "9")) {
+    //         filytered += char.toLowerCase();
+    //     }
+    // }
+
+    // return filytered === filytered.split("").reverse().join("")
 };
