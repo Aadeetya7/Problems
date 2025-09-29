a898f679ce428a1c9927e12f2eab10f8e0c8b1ac
Aditya_Mali <113601204+Aadeetya7@users.noreply.github.com>
2025-09-29 10:22:00 +0530

    Time: 0 ms (100%), Space: 55.9 MB (12.95%) - LeetHub

---
+++ 0874-backspace-string-compare/0874-backspace-string-compare.js
@@ -0,0 +1,70 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function (s, t) {
+    // // Optimal solution using two pointer 
+    // let i = s.length - 1;
+    // let j = t.length - 1;
+    // let skipS = 0;
+    // let skipT = 0;
+
+    // while (i >= 0 || j >= 0) {
+
+    //     while (i >= 0) {
+    //         if (s[i] === "#") {
+    //             skipS++;
+    //             i--;
+    //         }
+    //         else if (skipS > 0) {
+    //             skipS--;
+    //             i--;
+    //         }
+    //         else {
+    //             break;
+    //         }
+    //     }
+    //     while (t >= 0) {
+    //         if (t[j] === "#") {
+    //             skipS++;
+    //             j--;
+    //         }
+    //         else if (skipS > 0) {
+    //             skipS--;
+    //             j--;
+    //         }
+    //         else {
+    //             break;
+    //         }
+    //     }
+
+    //     const charS = i >= 0 ? s[i] : null;
+    //     const charT = j >= 0 ? t[j] : null;
+    //     if (charS !== charT) null;
+
+    //     i--;
+    //     j--
+    // }
+    // return true
+
+
+
+
+
+
+    // SOLVED UISNG STACK AS I was learning stack but this is not the optimal solution
+     function build(str){
+        let stack = [];
+        for(let char of str){
+            if(char !== "#"){
+                stack.push(char)
+            }
+            else if(stack.length > 0){
+                stack.pop();
+            }
+        }
+        return stack.join("")
+     }
+     return build(s) === build(t)
+};
