841ffb8d780e771261691fca78fc6f9d5d9042e1
Aditya_Mali <113601204+Aadeetya7@users.noreply.github.com>
2025-09-30 08:14:31 +0530

    Time: 0 ms (100%), Space: 55.8 MB (12.86%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.js
+++ 0874-backspace-string-compare/0874-backspace-string-compare.js
@@ -4,67 +4,64 @@
  * @return {boolean}
  */
 var backspaceCompare = function (s, t) {
-    // // Optimal solution using two pointer 
-    // let i = s.length - 1;
-    // let j = t.length - 1;
-    // let skipS = 0;
-    // let skipT = 0;
+    // Optimal solution using two pointer 
 
-    // while (i >= 0 || j >= 0) {
+    let i = s.length - 1;
+    let j = t.length - 1;
+    let skipS = 0;
+    let skipT = 0;
 
-    //     while (i >= 0) {
-    //         if (s[i] === "#") {
-    //             skipS++;
-    //             i--;
-    //         }
-    //         else if (skipS > 0) {
-    //             skipS--;
-    //             i--;
-    //         }
-    //         else {
-    //             break;
-    //         }
-    //     }
-    //     while (t >= 0) {
-    //         if (t[j] === "#") {
-    //             skipS++;
-    //             j--;
-    //         }
-    //         else if (skipS > 0) {
-    //             skipS--;
-    //             j--;
-    //         }
-    //         else {
-    //             break;
-    //         }
-    //     }
+    while (i >= 0 || j >= 0) {
+        while (i >= 0) {
+            if (s[i] == "#") {
+                skipS++;
+                i--;
+            }
+            else if (skipS > 0) {
+                skipS--;
+                i--
+            }
+            else {
+                break;
+            }
+        }
 
-    //     const charS = i >= 0 ? s[i] : null;
-    //     const charT = j >= 0 ? t[j] : null;
-    //     if (charS !== charT) null;
+        while (j >= 0) {
+            if (t[j] == "#") {
+                skipT++;
+                j--;
+            }
+            else if (skipT > 0) {
+                skipT--;
+                j--
+            }
+            else{
+                break
+            }
+        }
 
-    //     i--;
-    //     j--
-    // }
-    // return true
+        const charS = i >= 0 ? s[i] : null;
+        const charT = j >= 0 ? t[j] : null;
+        if(charS !== charT) return false;
 
-
-
-
+        i--;
+        j--; 
+    }
+    return true
 
 
     // SOLVED UISNG STACK AS I was learning stack but this is not the optimal solution
-     function build(str){
-        let stack = [];
-        for(let char of str){
-            if(char !== "#"){
-                stack.push(char)
-            }
-            else if(stack.length > 0){
-                stack.pop();
-            }
-        }
-        return stack.join("")
-     }
-     return build(s) === build(t)
+    // function build(str) {
+    //     let stack = [];
+    //     for (let char of str) {
+    //         if (char !== "#") {
+    //             stack.push(char)
+    //         }
+    //         else if (stack.length > 0) {
+    //             stack.pop();
+    //         }
+    //     }
+    //     return stack.join("")
+    // }
+    // return build(s) === build(t)
 };
