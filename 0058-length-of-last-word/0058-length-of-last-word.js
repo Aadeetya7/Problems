ba7f9a4aaf4b5109168ae528fd413b1444372ab6
Aditya_Mali <113601204+Aadeetya7@users.noreply.github.com>
2025-10-11 08:47:22 +0530

    Time: 0 ms (100%), Space: 53.6 MB (66.61%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-    return s.trim().split(" ").pop().length;
+    // return s.trim().split(" ").pop().length;
 
 
     //    More optimal here array is not created so less space complexity 
