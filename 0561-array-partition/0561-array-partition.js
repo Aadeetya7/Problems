95b2375e9f0038dde8e115f66c7baa090b83f85a
Aditya_Mali <113601204+Aadeetya7@users.noreply.github.com>
2025-10-22 09:04:38 +0530

    Time: 38 ms (83.6%), Space: 57.9 MB (99.2%) - LeetHub

--- 0561-array-partition/0561-array-partition.js
+++ 0561-array-partition/0561-array-partition.js
@@ -4,9 +4,24 @@
  */
 var arrayPairSum = function (nums) {
     nums.sort((a, b) => a - b);
-    let sum = 0
+
+    let sum = 0;
+
     for (let i = 0; i < nums.length; i += 2) {
-        sum += nums[i];
+        sum += nums[i]
     }
     return sum
+
+
+
+
+
+
+
+    // nums.sort((a, b) => a - b);
+    // let sum = 0
+    // for (let i = 0; i < nums.length; i += 2) {
+    //     sum += nums[i];
+    // }
+    // return sum
 };
