b1cdf0557fbcbaef62e805619f6c6a44e61a420e
Aditya_Mali <113601204+Aadeetya7@users.noreply.github.com>
2025-12-26 17:31:43 +0530

    Time: 1 ms (90.33%), Space: 55.3 MB (77.8%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.js
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -4,42 +4,42 @@
  */
 var thirdMax = function (nums) {
     // Recursion
-    function helper(index, distinctSet) {
-        if (index === nums.length) {
-            let sorted = Array.from(distinctSet).sort((a, b) => b - a)
-            return sorted.length >= 3 ? sorted[2] : sorted[0];
-        }
-        distinctSet.add(nums[index]);
-
-        return helper(index + 1, distinctSet)
-    }
-
-    return helper(0, new Set())
-    
-    // Optimal Sol
-    // let first = null, second = null, third = null;
-
-
-    // for (let num of nums) {
-    //     if (num === first || num === second || num === third) {
-    //         continue;
+    // function helper(index, distinctSet) {
+    //     if (index === nums.length) {
+    //         let sorted = Array.from(distinctSet).sort((a, b) => b - a)
+    //         return sorted.length >= 3 ? sorted[2] : sorted[0];
     //     }
+    //     distinctSet.add(nums[index]);
 
-    //     if (first === null || num > first) {
-    //         third = second;
-    //         second = first;
-    //         first = num;
-    //     }
-    //     else if (second === null || num > second) {
-    //         third = second;
-    //         second = num
-    //     }
-    //     else if (third === null || num > third) {
-    //         third = num;
-    //     }
+    //     return helper(index + 1, distinctSet)
     // }
 
-    // return third === null ? first : third;
+    // return helper(0, new Set())
+    
+    // Optimal Sol
+    let first = null, second = null, third = null;
+
+
+    for (let num of nums) {
+        if (num === first || num === second || num === third) {
+            continue;
+        }
+
+        if (first === null || num > first) {
+            third = second;
+            second = first;
+            first = num;
+        }
+        else if (second === null || num > second) {
+            third = second;
+            second = num
+        }
+        else if (third === null || num > third) {
+            third = num;
+        }
+    }
+
+    return third === null ? first : third;
 
 
     // // Brute Force
